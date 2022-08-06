1d02b98cd32c673e2bd34b7fa1a523d98bc66b9c
it21821172 <110707503+It21821172@users.noreply.github.com>
2022-08-06 09:30:16 +0000

    tutorial 1 is done

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+  int mark1,mark2,tot,avg;
+
+  printf("Enter mark: ");
+  scanf("%d", &mark1);
+
+  printf("Enter mark: ");
+  scanf("%d", &mark2);
+
+  tot = mark1+mark2;
+  avg = tot/2;
+
+  printf("Average= %d ", avg);
   
   return 0;
 }
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,19 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  int dist,tot;
+	
+	printf("Enter travel Distance: ");
+	scanf("%d",&dist);
+	
+	if(dist<30){
+		tot=dist*50;
+		printf("Total cost is: %d",tot);
+	}
+	else(dist>30){
+		tot=30*50+(dist-30)*40;
+		printf("Total cost is: %d",tot);
+	}
   
   return 0;
 }
